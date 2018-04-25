dfb5129d94e08fc0d1e35dc79b83eaa9a1789faa
Jacky Rusly <jackyrusly@gmail.com>
2018-04-25 09:37:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const schema = require('./schema');
+const schema = require('./src/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
